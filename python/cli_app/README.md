faaea6ebef6306e24db9bc3b7fe800e3363c1ce5
Diego Fleitas <diegofleitas98@outlook.es>
2024-10-16 15:43:02 -0300

    build skeleton to cli app

---
+++ python/cli_app/README.md
@@ -0,0 +1,1 @@
+## Project based on: https://www.youtube.com/playlist?list=PL7g1jYj15RUMeIY778b8lvgUzdRFnEniI
---
+++ python/cli_app/cli_app/app.py
@@ -0,0 +1,29 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Search for word in file")
+
+    parser.add_argument("-w", "--word", required=True, help="Word to be search for")
+
+    parser.add_argument("-f", "--file", required=True, help="Path to file that is to be searched")
+
+    parser.add_argument("-sa", "--search-algorithm", choices=["binary-search", "depth-first-search", "breadth-first-search"], required=True, help="The algorithm to be used to search the file")
+
+    parser.add_argument("-o", "--order", choices=["pre-order", "post-order", "level-order"], required=True, help="Order in wich to traverse the tree")
+
+    args = parser.parse_args()
+
+    if args.search_algorithm === "depth-first-search":
+        depth_first.search(args)
+        return
+    
+    if args.search_algorithm === "breadth-first-search":
+        breadth_first.search(args)
+        return
+    
+    if args.search_algorithm === "binary-search":
+        binary.search(args)
+        return
+
+if __name__ == "__main__":
+    main()
