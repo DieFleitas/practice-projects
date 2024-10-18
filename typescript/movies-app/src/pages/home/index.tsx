81dfb072001d1c7c481f8b3575f35cf9d6761ada
Diego Fleitas <diegofleitas98@outlook.es>
2024-10-18 14:42:16 -0300

    build home component

--- typescript/movies-app/src/pages/home/index.tsx
+++ typescript/movies-app/src/pages/home/index.tsx
@@ -1,10 +1,64 @@
-import React from 'react'
-import Layout from '../../Layout'
+import React, { SetStateAction, useState } from 'react';
+import Layout from '../../Layout';
+import { Box, InputAdornment, InputBase, Paper, Typography } from '@mui/material';
+import SearchIcon from "../../assets/icons/icon-search.svg"
 
 const Home = () => {
+  const [search, setSearch] = useState('');
+  const handleSearch = (e: { target: { value: SetStateAction<string> } }) => {
+    setSearch(e.target.value);
+  };
   return (
-    <Layout>Home</Layout>
-  )
-}
+    <Layout>
+      <Box>
+        <Paper
+          component="form"
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            borderRadius: 'default',
+            padding: 1,
+            backgroundColor: '#10141f',
+            border: 'none',
+          }}
+        >
+          <InputBase
+            placeholder="Search for movies or TV series"
+            sx={{ marginLeft: 1, flex: 1, color: 'white', border: 'none' }}
+            value={{ search }}
+            onChange={handleSearch}
+            startAdornment= {
+              <InputAdornment position='start'>
+                <img src={SearchIcon} alt="search icon" width={20} height={20} />
+              </InputAdornment>
+            }
+          />
+        </Paper>
+      </Box>
+      <Box py={2} px={4}>
+            {search === "" ? (
+              <Box width="100%">
+                <Box width="100%">
+                  <Typography variant='h5' component="h1" my={6} fontWeight={400}>
+                    Trending
+                  </Typography>
+                  <MovieTrendList trendingList={trendingList} />
+                </Box>
+                <Box width="100%">
+                  <Typography variant='h5' component="h1" my={6} fontWeight={400}>
+                    Recommended for you
+                  </Typography>
+                  <MovieList recommendedList={recommendedList} />
+                </Box>
+              </Box>
+            ) : (
+              <Box width="100%">
+                <Typography>Found</Typography>
+              </Box>
+            )}
+      </Box>
+    </Layout>
+  );
+};
 
-export default Home
+export default Home;
