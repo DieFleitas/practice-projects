6ab3c1404b8888e1a313c04052c9b72e0fea11a1
Diego Fleitas <diegofleitas98@outlook.es>
2024-10-28 14:48:52 -0300

    added test to repository layer

--- java/cloud-vendor-api/src/main/java/com/practice/cloud_vendor_api/repository/CloudVendorRepository.java
+++ java/cloud-vendor-api/src/main/java/com/practice/cloud_vendor_api/repository/CloudVendorRepository.java
@@ -4,6 +4,9 @@ import com.practice.cloud_vendor_api.model.CloudVendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CloudVendorRepository extends JpaRepository<CloudVendor, String> {
+    List<CloudVendor> findByVendorName(String vendorName);
 }
