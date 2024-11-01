cc71290197f9b030dcde5d04d8e7a2b5ee750216
Diego Fleitas <diegofleitas98@outlook.es>
2024-11-01 13:22:19 -0300

    added search, recommended & trending movies to home

--- typescript/movies-app/src/pages/home/index.tsx
+++ typescript/movies-app/src/pages/home/index.tsx
@@ -1,16 +1,26 @@
-import React, { SetStateAction, useState } from 'react';
+import React, { SetStateAction, useContext, useState } from 'react';
 import Layout from '../../Layout';
 import { Box, InputAdornment, InputBase, Paper, Typography } from '@mui/material';
-import SearchIcon from "../../assets/icons/icon-search.svg"
+import SearchIcon from '../../assets/icons/icon-search.svg';
 import MovieTrendList from '../../components/movie-list/movieTrendList';
 import MovieList from '../../components/movie-list';
 import { MovieDataType } from '../../assets/data';
+import { MovieContext } from '../../context/movie-context';
 
 const Home = () => {
   const [search, setSearch] = useState('');
-  const [searchList, setSearchList] = useState<MovieDataType[]>([])
+  const [searchList, setSearchList] = useState<MovieDataType[]>([]);
+  const { state } = useContext(MovieContext);
+  const { movies } = state;
+  const trendingList: any = movies.filter((item) => item.isTrending === true);
+  const recommendedList: any = movies.filter((item) => item.isTrending !== true);
+
   const handleSearch = (e: { target: { value: SetStateAction<string> } }) => {
     setSearch(e.target.value);
+    const newList = movies.filter((movie) =>
+      movie.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setSearchList(newList)
   };
   return (
     <Layout>
@@ -31,8 +41,8 @@ const Home = () => {
             sx={{ marginLeft: 1, flex: 1, color: 'white', border: 'none' }}
             value={{ search }}
             onChange={handleSearch}
-            startAdornment= {
-              <InputAdornment position='start'>
+            startAdornment={
+              <InputAdornment position="start">
                 <img src={SearchIcon} alt="search icon" width={20} height={20} />
               </InputAdornment>
             }
@@ -40,26 +50,26 @@ const Home = () => {
         </Paper>
       </Box>
       <Box py={2} px={4}>
-            {search === "" ? (
-              <Box width="100%">
-                <Box width="100%">
-                  <Typography variant='h5' component="h1" my={6} fontWeight={400}>
-                    Trending
-                  </Typography>
-                  <MovieTrendList trendingList={trendingList} />
-                </Box>
-                <Box width="100%">
-                  <Typography variant='h5' component="h1" my={6} fontWeight={400}>
-                    Recommended for you
-                  </Typography>
-                  <MovieList recommendedList={recommendedList} />
-                </Box>
-              </Box>
-            ) : (
-              <Box width="100%">
-                <Typography>Found</Typography>
-              </Box>
-            )}
+        {search === '' ? (
+          <Box width="100%">
+            <Box width="100%">
+              <Typography variant="h5" component="h1" my={6} fontWeight={400}>
+                Trending
+              </Typography>
+              <MovieTrendList trendingList={trendingList} />
+            </Box>
+            <Box width="100%">
+              <Typography variant="h5" component="h1" my={6} fontWeight={400}>
+                Recommended for you
+              </Typography>
+              <MovieList recommendedList={recommendedList} />
+            </Box>
+          </Box>
+        ) : (
+          <Box width="100%">
+            <Typography>Found</Typography>
+          </Box>
+        )}
       </Box>
     </Layout>
   );
