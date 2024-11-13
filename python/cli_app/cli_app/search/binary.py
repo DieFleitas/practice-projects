ffb62b5c1db7d61e02b2d5edcba087e4214c92ac
Diego Fleitas <diegofleitas98@outlook.es>
2024-11-13 13:19:29 -0300

    added binary search algorithm and binary search tree data structure

--- python/cli_app/cli_app/search/binary.py
+++ python/cli_app/cli_app/search/binary.py
@@ -1,5 +1,33 @@
 from data_structures.binary_tree import BinaryTree
 from data_structures.queue import Queue
 
+def binary_search(target, root):
+    if root:
+        if root.data == target:
+            return True
+        
+        if int(root.data) < int(target):
+            if binary_search(target, root.right):
+                return True
+            
+        if int(root.data) > int(target):
+            if binary_search(target, root.left):
+                return True
+
 def search(args):
-    pass
+    bst = BinaryTree()
+
+    print("Building tree...")
+    bst.create_bst_from_file(args.file)
+    print("Tree build!")
+
+    print("Searching tree...")
+
+    target = args.word
+
+    if binary_search(target, bst.root):
+        print("Word found!")
+        return
+    else:
+        print("Word not found :(")
+        return
