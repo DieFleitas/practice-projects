4c463d19818367f1f169d8de7e168b62a146a006
diegofleitas <diegofleitas98@outlook.es>
2025-10-05 00:00:36 -0300

    Variables, Op. basicas

---
+++ java/dsa-cp/src/main/java/Week1/Day1.java
@@ -0,0 +1,13 @@
+package main.java.Week1;
+
+public class Day1 {
+    public static void main(String[] args) {
+        double base = 8.5;
+        double altura = 4.2;
+
+        // Tu código aquí (calcula el área)
+        double area = base * altura;
+
+        System.out.println("Área del rectángulo: " + area);
+    }
+}
--- java/java-master-path/src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("🚀 Inicio de mi viaje Java - " + java.time.LocalDate.now());
-    }
-}
